b24912a3f883a6b90c23767f415135807302ca01
Vedansh Sahu <vedansh0020@gmail.com>
2026-07-03 10:00:30 +0530

    Basics

---
+++ src/main/java/com/edigest/myFirstProject/Hello.java
@@ -0,0 +1,22 @@
+package com.edigest.myFirstProject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello Fellas" ;
+    }
+    @GetMapping("/sum")
+    public int sum(@RequestParam int n,@RequestParam int m){
+        return m+n;
+    }
+    @GetMapping("/smthing")
+    public String xyz(){
+        return "Blah Blah Blah";
+    }
+}
+
